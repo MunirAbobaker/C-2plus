5b4bd25d27211a7731e54071696306e43e524349
Munir <munir.abobaker@sap.com>
2022-01-01 19:54:59 +0100

    finish commit

--- main.cpp
+++ main.cpp
@@ -9,182 +9,57 @@
 #include <cmath>
 
 using namespace std;
-int imGlobal = 0;
-const double PI = 3.142;
+// create prototype for fct
+double AddNumbers(double num1, double num2);
+// we receive a pointer
+// address of memory
+void AssignAge(int* pAge);
+void DoubleArray(int *arr, int size);
 int main(int argc, char** argv) {
-    cout << "hello wrold" << endl; // used name space above no std:: needed
-    std::cout << "Hello, World!" << std::endl;
-    bool married = true;
-    char myGrade = 'A';
-    cout << "Min unsigned short int " <<
-         numeric_limits<unsigned short int>::min()
-         << endl;
+    printf("%.1f + %.1f = %.1f", 5.0, 4.0,
+           AddNumbers(5,4));
+    cout << endl;
+    int age = 43;
+    // to pass addresses use & reference operator
+    AssignAge(&age);
+    cout << age << endl;
 
-    cout << "Max unsigned short int " <<
-         numeric_limits<unsigned short int>::max()
-         << endl;
-    cout << "Min short int " <<
-         numeric_limits<short int>::min()
-         << endl;
+    int age2 = 43;
+    int* pAge2 = NULL; // good practice to assign poitners to Null
+    // take addres of age2 and store it in pointer
+    pAge2 = &age2;
+    cout << "Address itself: " << pAge2 << endl;
+    cout << "value stored in address: " << *pAge2 << endl;
 
-    cout << "Max short int " <<
-         numeric_limits<short int>::max()
-         << endl;
-
-    cout << "Min  int " <<
-         numeric_limits<int>::min()
-         << endl;
-
-    cout << "max int " <<
-         numeric_limits< int>::max()
-         << endl;
-
-    cout << "Min long int " <<
-         numeric_limits<long int>::min()
-         << endl;
-
-    cout << "Max long int " <<
-         numeric_limits<long int>::max()
-         << endl;
-
-    // float
-
-    cout << "Min float " <<
-         numeric_limits<float>::min()
-         << endl;
-
-    cout << "Max float" <<
-         numeric_limits<float>::max()
-         << endl;
-    cout << "Min double " <<
-         numeric_limits<double>::min()
-         << endl;
-
-    cout << "Max double" <<
-         numeric_limits<double>::max()
-         << endl;
-
-    cout << "Min long double  " <<
-         numeric_limits<long double>::min()
-         << endl;
-
-    cout << "Max long double " <<
-         numeric_limits<long double>::max()
-         << endl;
-
-    // float are only precise up to 6 digits
-    float f1 = 1.1111111;
-    float f2 = 1.1111111;
-    printf("Sum = %.7f\n", (f1 + f2));// in the 7th decimal place we los precision
-    // double are precise up until 16
-    auto whatAmI = true; // automaticaly assign a value to this variable
-    cout << "int Byte: " << sizeof(int) << endl;
-    // char, decimal, 5 spaces, decimal, float, string
-    printf("%c %d %5d %.3f %s\n", 'A',
-           10, 5, 3.1234, "Hi");
-
-    string q1 = "Enter a Number: ";
-    string num1, num2;
-    cout << q1;
-    cin >> num1;
-    cout << "Enter Another Number; ";
-    cin >> num2;
-    int nNum1 = stoi(num1);
-    int nNum2 = stoi(num2); // convert string to int
-    // use stof for string to float
-    printf("%d + %d = %d\n", nNum1, nNum2,
-           (nNum1 + nNum2));
-
-    printf("%d %% %d = %d\n", nNum1, nNum2,
-           (nNum1 % nNum2));
-
-    int age43 = 43;
-    bool canIVote = (age43 >= 18) ? true : false;
-    // to make output boolean instead of 1 or 0
-    cout.setf(ios::boolalpha);
-    cout << "Can I vote: " << canIVote << endl;
-
-    // arrays
-    int arrNum[10] = {1};
-    int arrNums2[] = {1, 2, 3};
-    int arrNums3[5] = {8, 9};
-    cout << "1st Value " << arrNums3[0] << endl;
-    // number of layers, coloumns, rows
-    int arrNums4[2][2][2] = {{{1, 2}, {3, 4}},
-                             {{5, 6}, {7, 8}}};
-    cout << arrNums4[1][1][1] << endl;
-
-    // use vectors, when u don not know size
-    vector<int> vNums(2);
-    vNums[0] = 1;
-    vNums[1] = 2;
-    vNums.push_back(2); // put at end of v
-    cout << "Vector size: " << vNums.size() << endl;
-
-    vector<string> words;
-    stringstream ss("Some Random Words");
-    string word;
-    while (getline(ss, word, ' '))
+    int intArray[] = {1, 2, 3, 4};
+    int* pIntArray = intArray;
+    cout << "1st " << pIntArray << endl;
+    // get next address
+    pIntArray++;
+    cout << "2nd " <<pIntArray << endl;
+    // intArray is already a pointer, no need to use &
+    DoubleArray(intArray, 4);
+    for(int i = 0; i < 4; ++i)
     {
-        words.push_back(word);
+        cout << "Array " << intArray[i] << endl;
     }
-    for(auto i = 0; i < words.size(); ++i)
-    {
-        cout << words[i] << endl;
-    }
-
-    // shorter form for loop
-    int arr3[] = {1, 2, 3};
-    for (auto x: arr3) cout << x << endl;
-
-    // use random number from ctime
-    srand(time(NULL));
-    int secretNum = rand() % 11;
-    int guess = 0;
-    do
-    {
-        cout << "Guess the Numbeer : ";
-        cin >> guess;
-        if (guess > secretNum) cout << "Too Big\n";
-        if (guess < secretNum) cout << "Too Small\n";
-    } while (secretNum != guess);
-    cout << " You Guessed it" << endl;
-    // strings
-    string str1 = "I am a string";
-    cout << "1st: "<< str1[0] << endl;
-    cout << "Last: " << str1.back() << endl;
-    cout << "Length: " << str1.length() << endl;
-    string str2 = str1;
-    string str3(str2, 4); // copy string ab 4 char
-    string str4 = str1 + "and your not";
-    str4.append("!");
-    // earse from the 12th position to the end
-    str4.erase(12, str4.length() -1 );
-    cout << "New String " << str4 << endl;
-    if(str4.find("string") != string::npos)
-        cout << "String Index: " <<
-             str4.find("string") << endl;
-
-
-
     return 0;
 }
 
-class DepthFirstSearchAdjacencyListIterative
+// add funct after main
+double AddNumbers(double num1 = 0, double num2 = 0)
 {
-    static class Edge
-    {
-        int from, to, cost;
-    public: Edge(int from, int to, int cost)
-        {
-            this->from = from;
-            this-> to = to;
-            this-> cost =  cost;
-        };
-    };
+    return num1 + num2;
+}
+void AssignAge(int* pAge)
+{
+    // use derefrence operator to change value at adress
+    *pAge = 22;
+}
 
-    static int dfs(vector<int> graph, int start, int n)
-    {
-        int count = 0;
+void DoubleArray(int *arr, int size)
+{
+    for(int i = 0; i < size; ++i) {
+        arr[i] = arr[i] * 2;
     }
-};
+}
